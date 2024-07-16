1a50d4c6b84e4d9708a3a24854ef75e5c9641fb0
sleekcharly <c_ukasoanya@yahoo.com>
2024-07-16 12:08:31 +0100

    Created login page and loading layout

--- app/(admin)/page.tsx
+++ app/(admin)/page.tsx
@@ -1,5 +1,24 @@
-import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default function Home() {
-  return <main className="">Welcome to Botimize</main>;
+  return (
+    <main className="p-10 bg-white m-10 rounded-md w-full">
+      <h1 className="text-4xl font-light">
+        Welcome to{' '}
+        <span className="text-[#e32b1e] font-semibold">Botimize</span>
+      </h1>
+
+      <h2 className="mt-2 mb-10">
+        Your customizable AI chat agent that helps you manage your customer
+        conversations.
+      </h2>
+
+      <Link href="/create-chatbot">
+        <Button className="bg-[#e32b1e]">
+          Let&apos;s get started by creating your first chatbot
+        </Button>
+      </Link>
+    </main>
+  );
 }
